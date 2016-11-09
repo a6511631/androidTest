e2da98b241678dd11db34234b6718602fef1faab
mumuxuan <6511631@qq.com>
2016-11-09 15:51:03 +0800

    在develop分支中添加一个新功能

--- app/src/main/java/com/it520/androidtest/MainActivity.java
+++ app/src/main/java/com/it520/androidtest/MainActivity.java
@@ -1,2 +1,17 @@
 package com.it520.androidtest;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(getClass().getSimpleName(), "onCreate: " + "hahahah");
+        Log.d(getClass().getSimpleName(), "onCreate: " + "添加一个新功能");
+
+    }
+}
